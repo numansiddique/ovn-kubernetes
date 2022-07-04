d02355727b6fc1ad2a067833049e13ccd5d9b4ad
Numan Siddique <numans@ovn.org>
2022-07-04 17:37:00 -0400

    ovnkube-master: Use the join subnets generated by ovnkube-cluster-manager.
    ovnkube-cluster-manager generates the Join switch subnets for each zone
    and stores it in the node annotations. Use this and pass it to the
    logical switch manager.
    
    Signed-off-by: Numan Siddique <numans@ovn.org>

--- go-controller/pkg/ovn/master_test.go
+++ go-controller/pkg/ovn/master_test.go
@@ -107,6 +107,25 @@ func (n tNode) logicalSwitch(loadBalancerGroupUUID string) *nbdb.LogicalSwitch {
 	}
 }
 
+func getJoinSwitchSubnets() []*net.IPNet {
+	var joinSubnets []*net.IPNet
+	joinSubnetsConfig := []string{}
+	if config.IPv4Mode {
+		joinSubnetsConfig = append(joinSubnetsConfig, config.Gateway.V4JoinSubnet)
+	}
+	if config.IPv6Mode {
+		joinSubnetsConfig = append(joinSubnetsConfig, config.Gateway.V6JoinSubnet)
+	}
+	for _, joinSubnetString := range joinSubnetsConfig {
+		_, joinSubnet, err := net.ParseCIDR(joinSubnetString)
+		if err == nil {
+			joinSubnets = append(joinSubnets, joinSubnet)
+		}
+	}
+
+	return joinSubnets
+}
+
 /*
 func cleanupGateway(fexec *ovntest.FakeExec, nodeName string, nodeSubnet string, clusterCIDR string, nextHop string) {
 	const (
@@ -970,7 +989,7 @@ var _ = ginkgo.Describe("Gateway Init Operations", func() {
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
 		clusterController.SCTPSupport = true
-		clusterController.joinSwIPManager, _ = lsm.NewJoinLogicalSwitchIPManager(clusterController.nbClient, expectedNodeSwitch.UUID, []string{node1.Name})
+		clusterController.joinSwIPManager, _ = lsm.NewJoinLogicalSwitchIPManager(clusterController.nbClient, expectedNodeSwitch.UUID, []string{node1.Name}, getJoinSwitchSubnets())
 		_, _ = clusterController.joinSwIPManager.EnsureJoinLRPIPs(types.OVNClusterRouter)
 	})
 
@@ -1459,7 +1478,7 @@ func TestController_syncNodesRetriable(t *testing.T) {
 				sbClient,
 				record.NewFakeRecorder(0))
 
-			controller.joinSwIPManager, err = lsm.NewJoinLogicalSwitchIPManager(nbClient, "", []string{})
+			controller.joinSwIPManager, err = lsm.NewJoinLogicalSwitchIPManager(nbClient, "", []string{}, getJoinSwitchSubnets())
 			if err != nil {
 				t.Fatalf("%s: Error creating joinSwIPManager: %v", tt.name, err)
 			}
@@ -1546,7 +1565,7 @@ func TestController_deleteStaleNodeChassis(t *testing.T) {
 				sbClient,
 				record.NewFakeRecorder(0))
 
-			controller.joinSwIPManager, err = lsm.NewJoinLogicalSwitchIPManager(nbClient, "", []string{})
+			controller.joinSwIPManager, err = lsm.NewJoinLogicalSwitchIPManager(nbClient, "", []string{}, getJoinSwitchSubnets())
 			if err != nil {
 				t.Fatalf("%s: Error creating joinSwIPManager: %v", tt.name, err)
 			}
